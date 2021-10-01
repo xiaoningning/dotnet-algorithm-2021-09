043d8b6ac40f90433abefdf4abfea748a2356186
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-09-30 22:27:15 -0700

    Update 977. Squares of a Sorted Array.cs

--- src/977. Squares of a Sorted Array.cs
+++ src/977. Squares of a Sorted Array.cs
@@ -2,7 +2,7 @@ public class Solution {
     public int[] SortedSquares(int[] nums) {
         int n = nums.Length, l = 0, r = n - 1, i = n - 1;
         int[] ans = new int[n];
-        while (i >= 0) {
+        while (i >= 0) { // better than check l <= r
             int mx = Math.Abs(nums[r]) > Math.Abs(nums[l]) ? nums[r--] : nums[l++];
             ans[i--] = mx * mx;
         }

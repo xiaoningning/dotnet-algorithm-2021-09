9b70b4929c1688bf244ee820e08bafe610e2e76f
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-09-30 22:25:36 -0700

    Create 977. Squares of a Sorted Array.cs

---
+++ src/977. Squares of a Sorted Array.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int[] SortedSquares(int[] nums) {
+        int n = nums.Length, l = 0, r = n - 1, i = n - 1;
+        int[] ans = new int[n];
+        while (i >= 0) {
+            int mx = Math.Abs(nums[r]) > Math.Abs(nums[l]) ? nums[r--] : nums[l++];
+            ans[i--] = mx * mx;
+        }
+        return ans;
+    }
+}

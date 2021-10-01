630b395899640d8c01b07c3a68c6ee51c16f5fec
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-10-01 11:25:11 -0700

    Create 560. Subarray Sum Equals K.cs

---
+++ src/560. Subarray Sum Equals K.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    // hash table T:(n) Brute force T: O(n^2)
+    public int SubarraySum(int[] nums, int k) {
+        int ans = 0, sum = 0;
+        var m = new Dictionary<int,int>(){[0] = 1};
+        foreach (int n in nums) {
+            sum += n;
+            if (m.ContainsKey(sum - k)) ans += m[sum - k];
+            // k == 0 => update m after check sum - k
+            if (!m.ContainsKey(sum)) m[sum] = 0;
+            m[sum]++;
+        }
+        return ans;
+    }
+}

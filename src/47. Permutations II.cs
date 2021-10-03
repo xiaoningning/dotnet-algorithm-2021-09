df38acddd318d9ed94810a1e2e4b90e3550a9770
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-10-03 00:31:57 -0700

    Update 47. Permutations II.cs

--- src/47. Permutations II.cs
+++ src/47. Permutations II.cs
@@ -1,6 +1,7 @@
 public class Solution {
     // permutation is ordered v.s. combination is not
-    public IList<IList<int>> PermuteUnique(int[] nums) {
+    // T: O(n!)
+    public IList<IList<int>> PermuteUnique2(int[] nums) {
         int n = nums.Length;
         Array.Sort(nums); // it has duplicates
         var ans = new List<IList<int>>();
@@ -23,5 +24,46 @@ public class Solution {
         };
         DFS(new List<int>());
         return ans;
+    }
+    public IList<IList<int>> PermuteUnique(int[] nums) {
+        int n = nums.Length;
+        var ans = new List<IList<int>>();
+        Array.Sort(nums);
+        Action<int[], int,int> swap = (a, i,j) => {
+            int t = a[i]; a[i] = a[j]; a[j] = t;
+        };
+        Action<int,int[]> DFS = null;
+        DFS = (start,t) => {
+            if (start >= n) { ans.Add(new List<int>(t)); return;}
+            for (int i = start; i < n; i++) {
+                if (i != start && t[i] == t[start]) continue; // avoid duplicates
+                swap(t, start, i);
+                // new int[] at each level of DFS to keep start as the same
+                DFS(start + 1,(int[])t.Clone());
+            }
+        };
+        DFS(0, nums);
+        return ans;
+    }
+    // WRONG => c# hashset<list> not unique due to list is a reference type
+    public IList<IList<int>> PermuteUniqueX(int[] nums) {
+        int n = nums.Length;
+        // use set to remove duplicates (x)
+        var ans = new HashSet<IList<int>>();
+        if (n == 0) { ans.Add(new List<int>()); return ans.ToList(); }
+        int first = nums[0];
+        int[] tmp = new int[n-1];
+        Array.Copy(nums, 1, tmp, 0, n - 1);
+        var prev = PermuteUnique(tmp);
+        foreach (var l in prev) {
+            // <= l.Count => insert at last
+            for (int i = 0; i <= l.Count; i++) {
+                if (l.Any() && i < l.Count && l[i] == first) continue;
+                l.Insert(i,first);
+                ans.Add(new List<int>(l));
+                l.RemoveAt(i);
+            }
+        }
+        return ans.ToList();
     }
 }

de10dd94e2942dcc894be20a0fa5a878a1a9d3f0
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-10-04 23:05:59 -0700

    Update 212. Word Search II.cs

--- src/212. Word Search II.cs
+++ src/212. Word Search II.cs
@@ -1,6 +1,34 @@
 public class Solution {
+    // DFS without Trie => TLE
+    public IList<string> FindWords2(char[][] board, string[] words) {
+        int m = board.Length, n = board[0].Length;
+        int[,] dirs = new int[4,2]{{1,0},{-1,0},{0,1},{0, -1}};
+        Func<int,int,int,string,bool> DFS = null;
+        DFS = (i, j, x, s) => {
+            if (x == s.Length) return true;
+            if (i < 0 || i >= m || j < 0 || j >= n || s[x] != board[i][j]) return false;
+            char cur = board[i][j];
+            bool ans = false;
+            board[i][j] = '#';
+            for (int d = 0; d < 4; d++) ans |= DFS(i + dirs[d, 0], j + dirs[d, 1], x + 1, s);
+            board[i][j] = cur;
+            return ans;
+        };
+        var ans = new List<string>();
+        var dict = new HashSet<string>(words);
+        foreach (string w in dict) {
+            bool found = false;
+            for (int i = 0; i < m && !found; i++) 
+                for (int j = 0; j < n && !found; j++)
+                    if (DFS(i, j, 0, w)) { 
+                        ans.Add(w);
+                        found = true;
+                    }
+        }
+        return ans;
+    }
     // Trie + DFS => multiple source to destination
-    // T: O(sum(words) + 4^maxLength(words)) S: O(sum(words) + words)
+    // T: O(sum(words) + m*n*4^l) l:maxLen(words) S: O(sum(words) + words)
     public IList<string> FindWords(char[][] board, string[] words) {
         int m = board.Length, n = board[0].Length;
         int[,] dirs = new int[4,2]{{1,0},{-1,0},{0,1},{0, -1}};
@@ -17,27 +45,20 @@ public class Solution {
         };
         foreach (string w in words) buildTrie(w);
         var ans = new List<string>();
-        Action<TrieNode, int,int, int[,]> DFS = null;
-        DFS = (ptr,i,j, visited) => {
-            var nptr = ptr.children[board[i][j] - 'a'];
+        Action<TrieNode, int, int> DFS = null;
+        DFS = (ptr, i, j) => {
+            if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] == '#') return;
+            char cur = board[i][j];
+            var nptr = ptr.children[cur - 'a'];
             if (nptr == null) return;
             // continue search for overlapping words
             if (nptr.word != null) { ans.Add(nptr.word); nptr.word = null; }
-            for (int d = 0; d < 4; d++) {
-                int ni = i + dirs[d, 0], nj = j + dirs[d, 1];
-                if (ni < 0 || ni >= m || nj < 0 || nj >= n || visited[ni,nj] == 1) continue;
-                visited[ni,nj] = 1;
-                DFS(nptr, ni, nj, visited);
-                visited[ni,nj] = 0;
-            }
+            board[i][j] = '#';
+            for (int d = 0; d < 4; d++) DFS(nptr, i + dirs[d, 0], j + dirs[d, 1]);
+            board[i][j] = cur;
         };
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                var t = new int[m,n];
-                t[i,j] = 1;
-                DFS(root, i, j, t);
-            }
-        }
+        for (int i = 0; i < m; i++) 
+            for (int j = 0; j < n; j++) DFS(root, i, j);
         return ans;
     }
 }

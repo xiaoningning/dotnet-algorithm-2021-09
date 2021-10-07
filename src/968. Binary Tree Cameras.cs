bbca4f46a71bc47a565b5e6e3dde92adfa0037dc
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-10-07 13:32:33 -0700

    Update 968. Binary Tree Cameras.cs

--- src/968. Binary Tree Cameras.cs
+++ src/968. Binary Tree Cameras.cs
@@ -13,8 +13,28 @@
  */
 public class Solution {
     enum State { None = 0, Cam, Monitored }
-    // Top-down + node val as memo, otherwise TLE
+    // greedy recursion search based on state
+    // top down without memo
     public int MinCameraCover(TreeNode root) {
+        int ans = 0;
+        Func<TreeNode, State> f = null;
+        f = (node) => {
+            if (node == null) return State.Monitored;
+            var left = f(node.left);
+            var right = f(node.right);
+            // if one of none, then node needs cam
+            if (left == State.None || right == State.None) {
+                ans++;
+                return State.Cam;
+            }
+            // both left and right either cam or monitored, can not be none
+            if (left == State.Cam || right == State.Cam) return State.Monitored;
+            return State.None;
+        };
+        return f(root) == State.None ? ++ans : ans;
+    }
+    // Top-down to track min # of cam + node val as memo, otherwise TLE
+    public int MinCameraCover1(TreeNode root) {
         if (root == null) return 0;
         // state: 0 not monitored, 1 camera, 2 monitored
         Func<TreeNode, State, int> f = null;

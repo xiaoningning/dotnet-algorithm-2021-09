383f855f5181c5deed0e55f3341337bb5d810221
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-10-21 22:08:07 -0700

    Update 48. Rotate Image.cs

--- src/48. Rotate Image.cs
+++ src/48. Rotate Image.cs
@@ -3,6 +3,7 @@ public class Solution {
         int n = matrix.Length;
         // (i, j) <- (n-1-j, i) <- (n-1-i, n-1-j) <- (j, n-1-i)
         for (int i = 0; i < n / 2; i++) {
+            // (i, j) <- (n-1-j, i) := j -> i => j = i && j < n - 1 -i
             for (int j = i; j < n - 1 - i; j++) {
                 int t = matrix[i][j];
                 matrix[i][j] = matrix[n - 1 - j][i];

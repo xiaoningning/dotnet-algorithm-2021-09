6756430ec64a9aaabbf3b4a61a4b906844a800fd
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-11-05 16:21:53 -0700

    Update 74. Search a 2D Matrix.cs

--- src/74. Search a 2D Matrix.cs
+++ src/74. Search a 2D Matrix.cs
@@ -1,6 +1,8 @@
 public class Solution {
-    //binary search
+    // binary search 
+    // T: O(log(m*n)) S: O(1)
     public bool SearchMatrix(int[][] matrix, int target) {
+        if (matrix.Length == 0 || matrix[0].Length == 0) return false;
         int m = matrix.Length, n = matrix[0].Length;
         int i = 0, j = n - 1;
         while (i < m && j >= 0) {

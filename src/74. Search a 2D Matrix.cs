f34d694119f7b6f191cca4027b849bcd49c91dd4
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-11-05 16:17:57 -0700

    Create 74. Search a 2D Matrix.cs

---
+++ src/74. Search a 2D Matrix.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    //binary search
+    public bool SearchMatrix(int[][] matrix, int target) {
+        int m = matrix.Length, n = matrix[0].Length;
+        int i = 0, j = n - 1;
+        while (i < m && j >= 0) {
+            if (matrix[i][j] == target) return true;
+            else if (matrix[i][j] < target) i++;
+            else j--;
+        }
+        return false;
+    }
+}

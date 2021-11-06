e0f4ce3bf47dfec6d6e0de0a99ab2e79cdf479fc
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-11-05 17:07:42 -0700

    Update 451. Sort Characters By Frequency.cs

--- src/451. Sort Characters By Frequency.cs
+++ src/451. Sort Characters By Frequency.cs
@@ -1,5 +1,6 @@
 public class Solution {
-    public string FrequencySort(string s) {
+    // sorting
+    public string FrequencySort1(string s) {
         var freq = new Dictionary<char,int>();
         foreach (var c in s) {
             if (!freq.ContainsKey(c)) freq[c] = 0;
@@ -11,6 +12,30 @@ public class Solution {
         foreach (var kv in lst) { 
             int cnt = kv.Value;
             while (--cnt >= 0) ans += kv.Key; 
+        }
+        return ans;
+    }
+    // no sorting
+    public string FrequencySort(string s) {
+        int mxFreq = 0;
+        var freq = new Dictionary<char,int>();
+        foreach (var c in s) {
+            if (!freq.ContainsKey(c)) freq[c] = 0;
+            freq[c]++;
+            if (freq[c] > mxFreq) mxFreq = freq[c];
+        }
+        var strs = new string[mxFreq + 1];
+        Array.Fill(strs, "");
+        foreach (var kv in freq) {
+            strs[kv.Value] += kv.Key;
+        }
+        var ans = "";
+        for (int i = mxFreq; i > 0; i--) if (strs[i] != "")  {
+            var t = new string(strs[i].OrderBy(c => c).ToArray());
+            foreach (var c in t) { 
+                int cnt = i;
+                while (--cnt >= 0) ans += c;
+            }
         }
         return ans;
     }

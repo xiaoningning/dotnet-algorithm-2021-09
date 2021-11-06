0ff6eb20d34e0d629deecdf9ce5c941be9d0149d
Ning <xiaoningning@yahoo.com>
2021-11-05 17:14:40 -0700

    1712 remove console output

--- src/1712. Ways to Split Array Into Three Subarrays.cs
+++ src/1712. Ways to Split Array Into Three Subarrays.cs
@@ -1,18 +1,22 @@
-public class Solution {
+public class Solution
+{
     // two pointers: the right end of the middle array is in range [j, k – 1] 
     // and there are k – j choices for each i.
     // T: O(n) S: O(1)
-    public int WaysToSplit(int[] nums) {
-        int kMod = (int)Math.Pow(10,9) + 7, n = nums.Length;
+    public int WaysToSplit(int[] nums)
+    {
+        int kMod = (int)Math.Pow(10, 9) + 7, n = nums.Length;
         long ans = 0;
-        for (int i = 1; i < n; i++) nums[i] += nums[i-1];
-        for (int i = 0, j = 0, k = 0; i < n; i++) {
-            j = Math.Max(j, i+1);
+        for (int i = 1; i < n; i++) nums[i] += nums[i - 1];
+        for (int i = 0, j = 0, k = 0; i < n; i++)
+        {
+            j = Math.Max(j, i + 1);
             while (j < n - 1 && nums[i] > nums[j] - nums[i]) j++;
             k = Math.Max(k, j);
             while (k < n - 1 && nums[k] - nums[i] <= nums.Last() - nums[k]) k++;
             ans += k - j;
-        }Console.WriteLine(ans);
+        }
+        
         return (int)(ans % kMod);
     }
 }

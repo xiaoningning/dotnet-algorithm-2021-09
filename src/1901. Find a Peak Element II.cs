77a0728bec87695609c28fd41a158963dda065a9
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-11-05 18:08:52 -0700

    Create 1901. Find a Peak Element II.cs

---
+++ src/1901. Find a Peak Element II.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    // T: O(m log(n))
+    public int[] FindPeakGrid(int[][] mat) {
+        int m = mat.Length, n = mat[0].Length, lCol = 0, rCol = n - 1;
+        while (lCol <= rCol){
+            int mCol = lCol + (rCol - lCol) / 2, mxRow = 0;
+            for (int i = 0; i < m; i++) if (mat[i][mCol] > mat[mxRow][mCol]) mxRow = i;
+            if ((mCol == 0 ||  mat[mxRow][mCol] > mat[mxRow][mCol - 1])
+                && (mCol == n - 1 ||  mat[mxRow][mCol] > mat[mxRow][mCol + 1])) return new int[]{mxRow, mCol};
+            else if (mCol > 0 && mat[mxRow][mCol - 1] > mat[mxRow][mCol]) rCol = mCol - 1;
+            else lCol = mCol + 1;
+        }
+        return new int[]{};
+    }
+}

a3f5cd0d8384880f19f9e6eae4545b4394faaa4f
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-11-08 22:15:04 -0800

    Create 84. Largest Rectangle in Histogram.cs

---
+++ src/84. Largest Rectangle in Histogram.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    // monotonic stack.  similar to LC. 85
+    public int LargestRectangleArea(int[] heights) {
+        int n = heights.Length, ans = 0;
+        var st = new Stack<int>();
+        for (int i = 0; i <= n; i++) { // <= n :=> handle left boarder
+            int cur = i == n ? 0 : heights[i];
+            while (st.Any() && heights[st.Peek()] >= cur) {
+                ans = Math.Max(ans, heights[st.Pop()] * (st.Any() ? i - st.Peek() - 1 : i));
+            }
+            st.Push(i);
+        }
+        return ans;
+    }
+}

4922abe2cb57a85ae236a81fd1a3aa2c6e058696
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-11-10 15:46:21 -0800

    Create 251. Flatten 2D Vector.cs

---
+++ src/251. Flatten 2D Vector.cs
@@ -0,0 +1,44 @@
+public class Vector2D1 {
+    List<int> v = new List<int>(); 
+    int idx = 0;
+    public Vector2D1(int[][] vec) {
+        foreach (var x in vec) foreach (var n in x) v.Add(n);
+    }
+    
+    public int Next() {
+        return v[idx++];
+    }
+    
+    public bool HasNext() {
+        return idx < v.Count;
+    }
+}
+
+public class Vector2D {
+    int[][] data;
+    int x = 0;
+    int y = 0;
+    public Vector2D(int[][] vec) {
+        data = (int[][])vec.Clone();
+    }
+    
+    public int Next() {
+        HasNext();
+        return data[x][y++];
+    }
+    
+    public bool HasNext() {
+        while (x < data.Length && y == data[x].Length) {
+            ++x; 
+            y = 0;
+        }
+        return x < data.Length;
+    }
+}
+
+/**
+ * Your Vector2D object will be instantiated and called as such:
+ * Vector2D obj = new Vector2D(vec);
+ * int param_1 = obj.Next();
+ * bool param_2 = obj.HasNext();
+ */

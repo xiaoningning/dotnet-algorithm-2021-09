d440892dcb57a3e74fb797a1664783dc3be91868
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-11-09 17:21:42 -0800

    Update 980. Unique Paths III.cs

--- src/980. Unique Paths III.cs
+++ src/980. Unique Paths III.cs
@@ -6,7 +6,7 @@ public class Solution {
         int m = grid.Length, n = grid[0].Length, state = 0, ans = 0;
         int sx = -1, sy = -1;
         int[,] dirs = new int[,]{{0,1}, {0,-1}, {1,0}, {-1,0}};
-        var q = new Queue<(int,int,int)>();
+        
         for (int i = 0; i< m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) { sx = i; sy = j; }

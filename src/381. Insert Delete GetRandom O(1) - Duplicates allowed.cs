592dcc558ffec154fdbaecddfdad827d1b629401
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-11-16 22:44:14 -0800

    Create 381. Insert Delete GetRandom O(1) - Duplicates allowed.cs

---
+++ src/381. Insert Delete GetRandom O(1) - Duplicates allowed.cs
@@ -0,0 +1,40 @@
+public class RandomizedCollection {
+    List<int> nums = new List<int>();
+    Dictionary<int,HashSet<int>> m = new Dictionary<int,HashSet<int>>();
+    // O(1) with hashset
+    public RandomizedCollection() {}
+    
+    public bool Insert(int val) {
+        if (!m.ContainsKey(val)) m[val] = new HashSet<int>();
+        nums.Add(val);
+        m[val].Add(nums.Count - 1);
+        return m[val].Count == 1; // true if 1st time insert, false if multiple occurrences
+    }
+    
+    public bool Remove(int val) {
+        if (!m.ContainsKey(val) || m[val].Count == 0) return false;
+        int idx = m[val].First();
+        m[val].Remove(idx);
+        if (nums.Count - 1 != idx) {
+            int last = nums.Last();
+            nums[idx] = last;
+            m[last].Add(idx);
+            m[last].Remove(nums.Count - 1);
+        }
+        nums.RemoveAt(nums.Count - 1); // at last
+        return true;
+    }
+    
+    public int GetRandom() {
+        var rnd = new Random();
+        return nums[rnd.Next() % nums.Count];
+    }
+}
+
+/**
+ * Your RandomizedCollection object will be instantiated and called as such:
+ * RandomizedCollection obj = new RandomizedCollection();
+ * bool param_1 = obj.Insert(val);
+ * bool param_2 = obj.Remove(val);
+ * int param_3 = obj.GetRandom();
+ */

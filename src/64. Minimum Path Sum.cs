8ed068657fc47b9c8cc512cf74a911f18ea8d8de
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-11-21 09:35:40 -0800

    Update 64. Minimum Path Sum.cs

--- src/64. Minimum Path Sum.cs
+++ src/64. Minimum Path Sum.cs
@@ -25,7 +25,7 @@ public class Solution {
         return grid[m-1][n-1];
     }
     // recursion + memo
-    public int MinPathSum(int[][] grid) {
+    public int MinPathSum5(int[][] grid) {
         int m = grid.Length, n = grid[0].Length;
         int[,] memo = new int[m, n]; // grid is all positive val
         Func<int,int, int> DFS = null;
@@ -46,20 +46,48 @@ public class Solution {
         dist[0,0] = 0;
         var dirs = new int[,]{{1,0},{0,1}};
         int mn = Int32.MaxValue;
-        Action<int,int,int> DFS = null;
-        DFS = (i, j, w) =>{
+        var ans = new List<(int,int)>();
+        Action<int,int,int,List<(int,int)>> DFS = null;
+        DFS = (i, j, w, cur) =>{
             if (i == m - 1 && j == n - 1) {
-                if (w < mn) mn = w;
+                if (w < mn) { 
+                    mn = w; 
+                    ans = new List<(int,int)>(cur);
+                }
                 return;
             }
             for (int d = 0; d < 2; d++) {
                 int ni = i + dirs[d,0], nj = j + dirs[d, 1];
                 if (ni >= m || nj >= n || dist[ni,nj] < w + grid[ni][nj]) continue;
                 dist[ni,nj] = w + grid[ni][nj]; // pruning !!!
-                DFS(ni, nj, dist[ni,nj]);
+                cur.Add((ni,nj)); 
+                DFS(ni, nj, dist[ni,nj], cur);
+                cur.RemoveAt(cur.Count - 1); // back track the path
             }
         };
-        DFS(0,0,grid[0][0]);
+        DFS(0,0,grid[0][0], new List<(int, int)>() { (0, 0) });
         return mn;
+    }
+    // DP with path => PASSed !!!
+    public int MinPathSum(int[][] grid) {
+        int m = grid.Length, n = grid[0].Length;
+        int[,] dp = new int[m + 1, n + 1];
+        for (int i = 0; i <= m; i++) for (int j = 0; j <= n; j++) dp[i,j] = Int32.MaxValue;
+        dp[1,1] = grid[0][0];
+        for (int i = 1; i <= m; i++) 
+            for (int j = 1; j <= n; j++)
+                dp[i,j] = grid[i-1][j-1] + (i == 1 && j == 1 ? 0 : Math.Min(dp[i-1,j], dp[i, j-1]));
+        
+        // track path based on DP min value
+        var ans = new List<(int, int)>();
+        int x = m, y = n;
+        while (x > 0 && y > 0)
+        {
+            ans.Insert(0, (x - 1, y - 1));
+            if (dp[x - 1, y] < dp[x, y - 1]) x--;
+            else y--;
+        }
+        Console.WriteLine(string.Join(",", ans));
+        return dp[m,n];
     }
 }

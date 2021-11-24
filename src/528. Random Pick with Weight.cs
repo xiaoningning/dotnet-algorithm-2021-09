853a52f3141a753680fdee06a68d8db91efff10a
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-11-23 22:57:02 -0800

    Create 528. Random Pick with Weight.cs

---
+++ src/528. Random Pick with Weight.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    int[] sum;
+    public Solution(int[] w) {
+        sum = (int[])w.Clone(); // a shadow copy
+        for (int i = 1 ; i < sum.Length; i++) sum[i] += sum[i-1];
+    }
+    // The probability of picking an index i is w[i] / sum(w)
+    // 那么累加和数组为 [1, 4, 6]，整个的权重和为6，我们 rand() % 6，可以随机出范围 [0, 5] 内的数，
+    // 随机到 0 则为第一个点，随机到 1，2，3 则为第二个点，随机到 4，5 则为第三个点
+    public int PickIndex() {
+        var rnd = new Random();
+        int x = rnd.Next() % sum.Last(), l = 0, r = sum.Length - 1;
+        while (l < r) {
+            int m = l + (r -l) / 2;
+            if (sum[m] <= x) l = m + 1;
+            else r = m;
+        }
+        return r;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(w);
+ * int param_1 = obj.PickIndex();
+ */

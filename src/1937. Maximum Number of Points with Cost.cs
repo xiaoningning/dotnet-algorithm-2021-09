25c7b986ec355ce98b4236731269d592b5585dc0
Ning <xiaoningning@yahoo.com>
2021-12-01 11:07:34 -0800

    1937 update cmt

--- src/1937. Maximum Number of Points with Cost.cs
+++ src/1937. Maximum Number of Points with Cost.cs
@@ -1,15 +1,19 @@
-public class Solution {
+public class Solution
+{
     // Similar to LC. 64 LC. 931
+    // similar to LC. 85 LC. 84 left/right scan to save Time
     // T: O(m * n)
-    public long MaxPoints(int[][] points) {
+    public long MaxPoints(int[][] points)
+    {
         int m = points.Length, n = points[0].Length;
         var prev = new long[n]; // long since it can be big over int32
         for (int j = 0; j < n; j++) prev[j] = points[0][j];
-        for (int i = 1; i < m; i++) {
+        for (int i = 1; i < m; i++)
+        {
             long[] cur = new long[n], left = new long[n], right = new long[n];
             left[0] = prev[0];
             for (int j = 1; j < n; j++) left[j] = Math.Max(left[j - 1] - 1, prev[j]);
-            right[n-1] = prev[n - 1];
+            right[n - 1] = prev[n - 1];
             for (int j = n - 2; j >= 0; j--) right[j] = Math.Max(right[j + 1] - 1, prev[j]);
             for (int j = 0; j < n; j++) cur[j] = points[i][j] + Math.Max(left[j], right[j]);
             prev = cur;
@@ -21,15 +25,16 @@ public class Solution {
     // dp[i][j] = max(dp[i - 1][k] + k) + points[i][j] - j for all 0 <= k <= j
     // dp[i][j] = max(dp[i - 1][k] - k) + points[i][j] + j for all j <= k <= points[i].size() - 1
     // TLE
-    public long MaxPoints2(int[][] points) {
+    public long MaxPoints2(int[][] points)
+    {
         int m = points.Length, n = points[0].Length;
         var dp = new long[m, n];
-        for (int j = 0; j < n; j++) dp[0,j] = points[0][j];
+        for (int j = 0; j < n; j++) dp[0, j] = points[0][j];
         for (int i = 1; i < m; i++)
             for (int j = 0; j < n; j++)
                 for (int k = 0; k < n; k++)
-                    dp[i,j] = Math.Max(dp[i,j], dp[i - 1, k] - Math.Abs(k - j) + points[i][j]);
-        
+                    dp[i, j] = Math.Max(dp[i, j], dp[i - 1, k] - Math.Abs(k - j) + points[i][j]);
+
         long ans = -1;
         for (int j = 0; j < n; j++) ans = Math.Max(ans, dp[m - 1, j]);
         return ans;

1b452abfb07fea213b368a133233e7acace466d5
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-11-30 22:44:07 -0800

    Update 1937. Maximum Number of Points with Cost.cs

--- src/1937. Maximum Number of Points with Cost.cs
+++ src/1937. Maximum Number of Points with Cost.cs
@@ -7,7 +7,7 @@ public class Solution {
     // T: O(m * n)
     public long MaxPoints(int[][] points) {
         int m = points.Length, n = points[0].Length;
-        var prev = new long[n];
+        var prev = new long[n]; // long since it can be big over int32
         for (int j = 0; j < n; j++) prev[j] = points[0][j];
         for (int i = 1; i < m; i++) {
             long[] cur = new long[n], left = new long[n], right = new long[n];
@@ -19,5 +19,19 @@ public class Solution {
             prev = cur;
         }
         return prev.Max();
+    }
+    // TLE
+    public long MaxPoints2(int[][] points) {
+        int m = points.Length, n = points[0].Length;
+        var dp = new long[m, n];
+        for (int j = 0; j < n; j++) dp[0,j] = points[0][j];
+        for (int i = 1; i < m; i++)
+            for (int j = 0; j < n; j++)
+                for (int k = 0; k < n; k++)
+                    dp[i,j] = Math.Max(dp[i,j], dp[i - 1, k] - Math.Abs(k - j) + points[i][j]);
+        
+        long ans = -1;
+        for (int j = 0; j < n; j++) ans = Math.Max(ans, dp[m - 1, j]);
+        return ans;
     }
 }

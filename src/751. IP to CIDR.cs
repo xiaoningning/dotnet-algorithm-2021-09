26caa695b815ea9f1a9006b33c2ba06b1f7190e7
Ning <xiaoningning@yahoo.com>
2021-12-12 21:07:25 -0800

    update 751. ip to cidr ip2long by bit shift

--- src/751. IP to CIDR.cs
+++ src/751. IP to CIDR.cs
@@ -1,11 +1,14 @@
-public class Solution {
+public class Solution
+{
     // 255.0.0.7 -> 11111111 00000000 00000000 00000111 (IP form -> Integer form)
-    public IList<string> IpToCIDR2(string ip, int n) {
+    public IList<string> IpToCIDR2(string ip, int n)
+    {
         var start = ipToLong(ip);
         var ans = new List<string>();
         int cover = 0;
-        while (cover < n) {
-            long cntZero = countZero(start); 
+        while (cover < n)
+        {
+            long cntZero = countZero(start);
             // each 0 => two IPs, 2^cntZeros
             while (Math.Pow(2, cntZero) + cover > n) cntZero = cntZero - 1;
             int mask = (int)(32 - cntZero);
@@ -15,21 +18,32 @@ public class Solution {
         }
         return ans;
     }
-    long ipToLong(string ip) {
+    long ipToLong(string ip)
+    {
         long ans = 0;
         foreach (var p in ip.Split(".")) ans = ans * 256 + Int32.Parse(p);
         return ans;
     }
-    int countZero(long n) {
+    long ipToLongByBit(string ip)
+    {
+        var ipSegs = ip.Split(".");
+        long ans = 0;
+        for (int i = 0; i < 4; i++) ans += Int32.Parse(ipSegs[i]) << (8 * (3 - i)); // << 24, << 16, << 8, << 0
+        return ans;
+    }
+    int countZero(long n)
+    {
         if (n == 0) return 32; // handling 0.0.0.0 case.
         int ans = 0;
-        while (n > 0 && (n % 2 == 0)) {
+        while (n > 0 && (n % 2 == 0))
+        {
             ans += 1;
             n = n / 2;
         }
         return ans;
     }
-    string longToIp(long n) {
+    string longToIp(long n)
+    {
         return $"{(n >> 24) & 255}.{(n >> 16) & 255}.{(n >> 8) & 255}.{n & 255}";
     }
     public IList<string> IpToCIDR(string startIp, int n)
